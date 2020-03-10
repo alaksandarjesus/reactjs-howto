412784069bc183988ed87752c20375f85813fae5
alaksandarjesus <alaksandarjesus@yahoo.co.in>
2020-03-10 18:39:59 +0530

    added form handling

--- src/howto.js
+++ src/howto.js
@@ -1,18 +1,32 @@
 import React from "react";
 
 const html$ = <div>I am rendered from outside the component</div>;
-
+const lists = ["Learn React", "Code simple todo application"];
+const lists$ = lists.map(list => {
+  return <li>{list}</li>;
+});
 export default class Howto extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { aka: "Beta" };
+    this.state = { aka: "Beta", value: "" };
     this.changeAkaHandler = this.changeAkaHandler.bind(this);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
-    alert("i am executed whenever component is mounted");
+    // alert("i am executed whenever component is mounted");
   }
   changeAkaHandler() {
     this.setState({ aka: "Gamma" });
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
   }
   render() {
     return (
@@ -32,6 +46,20 @@ export default class Howto extends React.Component {
           <button onClick={this.props.onStateLiftedUp}>State Lift up</button>
         </p>
         <p>{html$}</p>
+        <div>{lists$}</div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name:
+              <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
